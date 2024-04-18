220050eb12aab5e7ca535e24ee723f76c48193e0
Carlos Oliveira <cadu.oliveira@yahoo.com.br>
2024-04-17 23:04:21 -0300

    Esqueci de salvar arquivo e commitei

--- src/imagens.js
+++ src/imagens.js
@@ -1,4 +1,5 @@
 export default imagens = {
     menu: require('./../assets/menu.png'),
-    real: require('./../assets/real.png')
+    real: require('./../assets/real.png'),
+    onlineshopping: require('./../assets/online-shopping.png')
 }
