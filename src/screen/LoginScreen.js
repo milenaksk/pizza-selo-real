74dfef2640912ad36eab7c17de34ba7e34537b89
MILENA CASTOR <milena.castor@paycredex.com.br>
2024-06-13 16:13:33 -0300

    react is a piece of heaven

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginButton = () => (
+  <View style={styles.loginButtonContainer}>
+    <Button title="ENTRAR" onPress={() => {}} color="#8B0000"/>
+  </View>
+);
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
@@ -7,9 +13,10 @@ const LoginScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>Login</Text>
       <TextInput
         style={styles.input}
-        placeholder="Email"
+        placeholder="E-mail"
         onChangeText={(text) => setEmail(text)}
         value={email}
         keyboardType="email-address"
@@ -22,7 +29,7 @@ const LoginScreen = () => {
         value={password}
         secureTextEntry={true}
       />
-      <Button title="Login" onPress={() => {}} />
+      <LoginButton></LoginButton>
     </View>
   );
 };
@@ -34,13 +41,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 20,
   },
+  header: {
+    fontSize: 40,
+    marginBottom: 33,
+  },
   input: {
     height: 40,
     width: '100%',
     borderColor: '#8B0000',
-    borderWidth: 1,
+    borderWidth: 2,
+    borderRadius: 5, // bordas arredondadas
     marginBottom: 20,
     paddingHorizontal: 10,
+    backgroundColor: '#D3D3D3',
+  },
+  loginButtonContainer: {
+    borderRadius: 10,
+    padding: 10,
+    overflow: 'hidden', // bordas arredondadas
   },
 });
 
