78109a86c114022d70ba448d8c058b0c93ee7a77
MILENA CASTOR <milena.castor@paycredex.com.br>
2024-06-17 11:25:53 -0300

    sombra atras do container

--- src/component/InicialBanner.js
+++ src/component/InicialBanner.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const InicialBanner = () => {
   return (
     <View style={styles.bannerContainer}>
-      <Text style={styles.bannerText}>Sua pizza favorita está aqui!</Text>
+      <Text style={styles.bannerText}>Escolha um tamanho</Text>
     </View>
   );
 };
@@ -13,19 +13,14 @@ const styles = StyleSheet.create({
   bannerContainer: {
     flex: 1,
     backgroundColor: '#e0e0e0', // cor de fundo do banner
-    marginTop: 10, // espaço de 10px abaixo da menubar
+    marginTop: 20, // espaço de 10px abaixo da menubar
     marginStart: 10,
     marginEnd: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
+    elevation: 4, // Essa propriedade adiciona sombra no Android
   },
   bannerText: {
+    fontWeight: 'bold',
+    padding:20,
     color: '#8B0000', // cor do texto do banner 
     fontSize: 36, // tamanho da fonte
     textAlign: 'center', // centalizar texto
